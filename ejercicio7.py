21932b7a02649692fc1388b5770ef3dd5e5c4739
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-09 22:03:03 -0500

    Add files via upload

---
+++ ejercicio7.py
@@ -0,0 +1,2 @@
+radio  =  float ( input ( "Ingrese el radio del círculo:" ))
+print ( "El area del círculo es" , 3.14159  * ( radio  **  2 ), "y el perimetro del círculo es" , 2  *  3.14159  *  radio )
